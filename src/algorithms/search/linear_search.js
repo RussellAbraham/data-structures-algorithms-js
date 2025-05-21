172c1def988b5a3879f78bc55d8bda80d5518da6
Russell Abraham <russell_a@live.com>
2025-05-20 21:46:41 -0700

    Updated linear search scripts for browser and modules

--- src/algorithms/search/linear_search.js
+++ src/algorithms/search/linear_search.js
@@ -5,11 +5,9 @@
  * @returns {number} - Index of the target, or -1 if not found.
  */
 
-function linearSearch(arr, val) {
+export default function linearSearch(arr, val) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === val) return i;
   }
   return -1;
 }
-
-// linearSearch([34,51,1,2,3,45,56,687], 100);
