bf6ba2f11cf7b463bb618af7b4ace22afdb9d2d3
Russell Abraham <russell_a@live.com>
2025-05-20 20:12:00 -0700

    Added linear search demo page

--- src/algorithms/search/linear_search.js
+++ src/algorithms/search/linear_search.js
@@ -1,8 +1,15 @@
-function linearSearch(arr, val){
-    for(var i = 0; i < arr.length; i++){
-        if(arr[i] === val) return i;
-    }
-    return -1;
+/**
+ * Performs a linear search.
+ * @param {Array} arr - The array to search.
+ * @param {*} val - The value to find.
+ * @returns {number} - Index of the target, or -1 if not found.
+ */
+
+export default function linearSearch(arr, val) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) return i;
+  }
+  return -1;
 }
 
-linearSearch([34,51,1,2,3,45,56,687], 100)
+// linearSearch([34,51,1,2,3,45,56,687], 100);
